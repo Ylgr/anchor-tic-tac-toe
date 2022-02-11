45f00da4cafaf6bfb2d4751a0cf89305e98d74e6
ylgr <quyen.luminous@gmail.com>
2022-02-11 15:41:58 +0700

    (+) play func

--- tests/tic-tac-toe.ts
+++ tests/tic-tac-toe.ts
@@ -1,7 +1,8 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { TicTacToe } from '../target/types/tic_tac_toe';
-import { expect } from 'chai';
+import chai from 'chai';
+const {expect} = chai;
 describe('tic-tac-toe', () => {
 
   // Configure the client to use the local cluster.
@@ -30,5 +31,56 @@ describe('tic-tac-toe', () => {
     expect(gameState.board)
         .to
         .eql([[null,null,null],[null,null,null],[null,null,null]]);
+
+    await play(
+        program,
+        gameKeypair.publicKey,
+        playerOne,
+        {row: 0, column: 0},
+        2,
+        { active: {}, },
+        [
+          [{x:{}},null,null],
+          [null,null,null],
+          [null,null,null]
+        ]
+    );
+
+    try {
+      await play(
+          program,
+          gameKeypair.publicKey,
+          playerTwo,
+          {row: 5, column: 1}, // out of bounds row
+          4,
+          { active: {}, },
+          [
+            [{x:{}},{x: {}},null],
+            [{o:{}},null,null],
+            [null,null,null]
+          ]
+      );
+      // we use this to make sure we definitely throw an error
+      chai.assert(false, "should've failed but didn't ");
+    } catch (error) {
+      expect(error.code).to.equal(6000);
+    }
   });
+
+  async function play(program, game, player,
+                      tile, expectedTurn, expectedGameState, expectedBoard) {
+    await program.rpc.play(tile, {
+      accounts: {
+        player: player.publicKey,
+        game
+      },
+      signers: player instanceof (anchor.Wallet as any) ? [] : [player]
+    });
+    const gameState = await program.account.game.fetch(game);
+    expect(gameState.turn).to.equal(expectedTurn);
+    expect(gameState.state).to.eql(expectedGameState);
+    expect(gameState.board)
+        .to
+        .eql(expectedBoard);
+  }
 });
